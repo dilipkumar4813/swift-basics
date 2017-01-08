16088e90a36c336687cbfb3c560161d23515d549
dilipkumar4813 <dilipkumar4813@gmail.com>
2017-01-08 19:36:59 +0530

    numbers

---
+++ number.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var a: Int = 2
+var b: Int = 3
+
+print(a," ",b)
+
+var sum = a + b
+print("sum \(sum)")
+
+var diff = a - b
+print("sum \(diff)")
+
+var mul = a * b
+print("sum \(mul)")
+
+var div = a / b
+print("sum \(div)")
+
+var mod = a % b
+print("sum \(mod)")
+
+var pi: Float = 3.14
+print("Value of pi \(pi)")
+
+
